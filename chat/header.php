eb62ef0a3d1a5094862d086968627eb3f730f3a9
ideawu <ideawu@local>
2014-05-06 09:42:19 +0800

    udpate

--- chat/header.php
+++ chat/header.php
@@ -2,9 +2,9 @@
 <html>
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>test</title>
-	<meta name="keywords" content="" />
-	<meta name="description" content="" />
+	<title>iComet Example Chat</title>
+	<meta name="keywords" content="comet, ajax server push, long-polling, comet server, chat" />
+	<meta name="description" content="icomet is a open source C1000K comet server built with libevent, providing full server-push solution of both comet server and js client API." />
 	<style type="text/css">
 	body{
 		padding-top: 0;
